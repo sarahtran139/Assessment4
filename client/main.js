b102ec7f9961fcd65624f5d406c1504043fc0b6e
sarahtran139@gmail.com <sarahtran139@gmail.com>
2023-07-16 22:06:04 -0400

    assessment4

---
+++ client/main.js
@@ -0,0 +1,34 @@
+//1st and 2nd feature
+const complimentBtn = document.getElementById("complimentButton");
+const fortuneBtn = document.getElementById("fortuneButton");
+
+const getCompliment = () => {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+      .then(res => {
+        const data = res.data;
+        alert(data);
+      });
+};
+
+complimentBtn.addEventListener('click', getCompliment);
+fortuneBtn.addEventListener('click', getFortune);
+
+
+
+//3rd feature
+
+
+
+
+
+//4th feature
+
+
+
---
+++ server/controller.js
@@ -0,0 +1,38 @@
+
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = [
+          "A beautiful, smart, and loving person will be coming into your life.",
+          "Your life will be filled with love and happiness.",
+          "Opportunities are coming your way. Be ready to seize them!",
+          "A dream you have will come true soon.",
+          "You will find great success in your future endeavors."
+        ];
+        const randomIndex = Math.floor(Math.random() * fortunes.length);
+        const randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    },
+}
+
+   
+    
+  
+
+
+
+
+
+
+
+     
